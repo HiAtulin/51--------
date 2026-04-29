54789e689ed6f89c3f290993406f097044ec402e
图阿里 <zylyw123@qq.com>
2026-04-29 19:10:54 +0800

    实现串口驱动

--- src/Dri/Dri_UART.c
+++ src/Dri/Dri_UART.c
@@ -1,5 +1,14 @@
 #include "Dri_UART.h"
 
+
+static bit s_is_transmited_byte = 1; // 发送完成标志
+static bit s_is_received_byte = 0;  // 接收完成标志
+
+
+static u8 s_received_bytes[16]; // 接收缓冲区
+static u8 s_received_bytes_count = 0; // 接收缓冲区计数器
+static bit s_is_received_bytes = 0; 
+
 void Dri_UART_Init(void)
 {
     // 设置串口模式
@@ -30,18 +39,72 @@ void Dri_UART_Init(void)
 
 void Dri_UART_TransmitByte(u8 byte)
 {
+    while (s_is_transmited_byte == 0)
+    {
+
+    }
+    SBUF = byte; // 将数据写入SBUF寄存器，开始发送
+    s_is_transmited_byte = 0; // 发送完成标志清零，等待中断服务函数设置为1
+    
 }
 
 void Dri_UART_TransmitBytes(u8 *bytes)
 {
+    u8 ch;
+    while (ch = *bytes++) 
+    {
+        Dri_UART_TransmitByte(ch); // 发送每个字符
+    }
 }
 
 bit Dri_UART_ReceiveByte(u8 *byte)
 {
-    return bit();
+    if (s_is_received_byte == 1)
+    {
+        *byte = SBUF; // 读取接收到的数据
+        s_is_received_byte = 0; // 清除接收完成标志
+        return 1; // 返回接收成功
+    }
+    return 0; // 返回接收失败
 }
 
 bit Dri_UART_ReceiveBytes(u8 *bytes)
 {
-    return bit();
+    u8 i;
+    if (s_is_received_bytes == 1)
+    {
+        for (i = 0; i < s_received_bytes_count; i++)
+        {
+            bytes[i] = s_received_bytes[i]; // 将接收到的数据复制到用户提供的缓冲区
+        }
+        bytes[s_received_bytes_count] = '\0'; // 添加字符串结束标志
+        s_received_bytes_count = 0; // 清除接收缓冲区计数器
+        s_is_received_bytes = 0; // 清除接收完成标志
+        return 1; // 返回接收成功
+    }
+    return 0; // 返回接收失败
+    
+}
+
+
+// 串口中断服务函数
+void Dri_UART_Handler(void) interrupt 4
+{
+    if (TI) // 发送完成中断
+    {
+        TI = 0; // 清除发送完成标志
+        // 手动记录，表示发送完成
+        s_is_transmited_byte = 1;
+    }
+    if (RI) // 接收完成中断
+    {
+        RI = 0; // 清除接收完成标志
+        // 手动记录，表示接收完成
+        s_is_received_byte = 1;
+        if (SBUF !='\n' && s_received_bytes_count < 15){
+            s_received_bytes[s_received_bytes_count++] = SBUF; // 将接收到的数据存入缓冲区
+        }else{
+            s_is_received_bytes = 1;
+        }
+    }
 }
